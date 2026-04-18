5d34c16dcd1f2c543f338f2f3a7c9591b09a62a7
gilior <121297116+gilior@users.noreply.github.com>
2026-04-18 19:57:13 +0000

    more

--- songs_manager/all_right.py
+++ songs_manager/all_right.py
@@ -5,8 +5,8 @@ OUTPUT_DIR = r"C:\Users\liorgishry\OneDrive - Microsoft\Pictures\all_right"
 
 def harvest() -> None:
     os.makedirs(OUTPUT_DIR, exist_ok=True)
-    for i in range(0, 34675590):
-        padded = str(34675590-i).zfill(8)
+    for i in range(0, 34669509):
+        padded = str(34669509-i).zfill(8)
         link = f"https://allright.com/og/{padded}_null_0_0.jpg"
         response = requests.get(link)
         if response.status_code == 200:
